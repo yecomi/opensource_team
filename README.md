a08ad713cd21e84811652a0badb7ffc7b87c7e63
yena <flosxna@naver.com>
2026-05-10 22:00:07 +0900

    Add Flask recommendation API

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # opensource_team
+
+##실행방법
+
+##Git규칙 || 꼭 해주기!
+1. 작업 전: git pull
+2. 작업 후: git add → commit → push
+3. main 직접 수정 최소화 
--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,84 @@
 from flask import Flask, request, jsonify
 
+#서버 객체 형성//app 생성
 app = Flask(__name__)
+
+
+recipes = [
+    {
+        "name": "김치볶음밥",
+        "ingredients": ["김치", "밥", "계란"]
+    },
+    {
+        "name": "계란볶음밥",
+        "ingredients": ["계란", "밥", "대파"]
+    },
+    {
+        "name": "토스트",
+        "ingredients": ["식빵", "계란", "버터"]
+    }
+]
+
 
 @app.route('/')
 def home():
-    return "Server running"
+    return "server running"
 
+
+
+#/recommanend로 요청 받겠다.=>주소 만들기
 @app.route('/recommend', methods=['POST'])
 def recommend():
+    
     data = request.json
-    return jsonify({"result": ["김치볶음밥", "계란말이"]})
+    
+    
+    user_ingredients = data.get('ingredients')
+    
+    results = []
+    
+    for recipe in recipes:
+        
+        score = 0
+        
+        for ingredient in user_ingredients:
+            if ingredient in recipe["ingredients"]:
+                score += 1
+
+        results.append({
+            "name": recipe["name"],
+            "score": score
+        })
+    
+    results.sort(key=lambda x: x["score"], reverse=True)
+
+    return jsonify(results)
+
+
+
+@app.route('/test')
+def test():
+    
+    user_ingredients = ["계란","밥"]
+    
+    results = []
+    
+    for recipe in recipes:
+
+        score = 0
+
+        for ingredient in user_ingredients:
+
+            if ingredient in recipe["ingredients"]:
+                score += 1
+
+        results.append({
+            "name": recipe["name"],
+            "score": score
+        })
+
+    results.sort(key=lambda x: x["score"], reverse=True)
+
+    return jsonify(results)
 
 app.run(debug=True)
